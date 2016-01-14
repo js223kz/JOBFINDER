3ece927b17b02a3773e90eaa237e40a636e087dc
js223kz <js223kz@student.lnu.se>
2016-01-14 17:19:02 +0100

    Getting city and county from user position

--- controllers/startController.js
+++ controllers/startController.js
@@ -1,20 +1,50 @@
-app.controller('StartController', function($geolocation, $scope){
+app.controller('StartController', function($geolocation, $scope, $http){
     
-   
+    $scope.userPos = {
+        lat: undefined,
+        lng: undefined,
+        city: undefined,
+        county: undefined,
+    }
     $scope.$geolocation = $geolocation;
+    $scope.baseUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=';
+   
 
     // basic usage
     $geolocation.getCurrentPosition().then(function(location) {
-      $scope.location = location
+        $scope.location = location;
+        $scope.lat = $scope.location.coords.latitude;
+        $scope.lng = $scope.location.coords.longitude;
+        
+        $http.get($scope.baseUrl + $scope.lat + ',' + $scope.lng)
+            .success(function(response){
+                
+            $scope.getCountyCode(response, $scope.lat, $scope.lng);
+            
+            }).error(function(error){
+                return null;
+        });         
     });
 
-    // regular updates
-    $geolocation.watchPosition({
-      timeout: 60000,
-      maximumAge: 2,
-      enableHighAccuracy: true
-    });
-    $scope.coords = $geolocation.position.coords; // this is regularly updated
-    $scope.error = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
-  
+    $scope.error = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs*/ 
+    
+    $scope.getCountyCode = function(response, lat, lng){
+        
+         //getting name of county   
+        $scope.countyPosition = response["results"].length -2;
+        $scope.county = response["results"][$scope.countyPosition].formatted_address;
+        $scope.userPos.county = $scope.county.split(',')[0];
+        
+        //getting name of city
+        $scope.cityPosition = response["results"].length -4;
+        $scope.city = response["results"][$scope.cityPosition].formatted_address;
+        $scope.userPos.city = $scope.city.split(',')[0];
+        
+        $scope.userPos.lat = lat;
+        $scope.userPos.lng = lng;
+
+        localStorage.setItem("userPosition", $scope.userPos);
+    };
 });
+
+ 
